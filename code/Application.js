449902f3c96fd95dff2cd5db169e80deb29355f4
Ken Tabor <ken@katworksgames.com>
2012-12-24 09:00:46 -0600

    smarter collection fetch implementing its own parse

--- code/Application.js
+++ code/Application.js
@@ -17,17 +17,7 @@ var Application = (function($) {
 	function getListing() {
 		showsListing = new TS.collection.ProgramList();
 		showsView = new TS.view.ProgramInfo({collection: showsListing});
-
-		$.ajax({
-			url: 'http://www.bbc.co.uk/tv/programmes/genres/drama/scifiandfantasy/schedules/upcoming.json',
-			dataType: 'json',
-			success: function(data) {
-				showsListing.reset(data.broadcasts);
-			},
-			error: function() {
-				console.log('service request error');
-			}
-		});
+		showsListing.fetch();
 	}
 
 
--- code/collections/ProgramListCollection.js
+++ code/collections/ProgramListCollection.js
@@ -2,6 +2,11 @@ TS.collection.ProgramList = Backbone.Collection.extend({
 
 	model: TS.model.ProgramInfo,
 
-	url: 'http://www.bbc.co.uk/tv/programmes/genres/drama/scifiandfantasy/schedules/upcoming.json'
+	url: 'http://www.bbc.co.uk/tv/programmes/genres/drama/scifiandfantasy/schedules/upcoming.json',
+
+	parse: function(response) {
+		return response.broadcasts;
+
+	}
 
 });
